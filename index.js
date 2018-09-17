5f52daa96e77181e32e38ae7ec91f08c88d7ac4c
Jason Dufrasne <jason.dufrasne@octopix.be>
2018-09-17 15:40:53 +0200

    Enable loose mode for the @babel/plugin-proposal-class-properties plugin to ensure compatibility with the @babel/plugin-proposal-decorators plugin.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = function() {
     return {
         plugins: [
             ['@babel/plugin-proposal-decorators', { legacy: true }],
-            '@babel/plugin-proposal-class-properties',
+            ['@babel/plugin-proposal-class-properties', { loose: true }],
             '@babel/plugin-transform-classes',
             ['@babel/plugin-transform-regenerator', { asyncGenerators: false }],
         ],
